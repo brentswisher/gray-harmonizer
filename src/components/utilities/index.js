809f90363e3daec3a5769a2065552a16ac9d3c3e
Brent Swisher <brent@brentswisher.com>
2018-11-14 23:42:40 -0500

    Add expandHexCode functionality

--- src/components/utilities/index.js
+++ src/components/utilities/index.js
@@ -1,10 +1,11 @@
-/**
-*	Expands a 3 digit hex to it's six digit equivalent
-**/
-export function expandHexCode(val) {
-	return 'todo';
+// Expands a 3 digit hex to it's six digit equivalent
+export function expandHexCode(shortCode) {
+	if(shortCode.charAt(0) === "#"){
+		shortCode = shortCode.substring(1,shortCode.length);
+	}
+	const hexArray = shortCode.toUpperCase().split('');
+	return hexArray[0]+hexArray[0]+hexArray[1]+hexArray[1]+hexArray[2]+hexArray[2];
 }
-
 
 // Take in a hex sting and convert it to an array of [r,g,b]
 export function hexToRGB(hexColor){
@@ -12,7 +13,6 @@ export function hexToRGB(hexColor){
 		hexColor = hexColor.substring(1,hexColor.length);
 	}
 	hexColor = hexColor.toUpperCase();
-	console.log(hexColor);
 	return hexColor.match(/[0-9A-F]{2}/g).map((color) => (
 		parseInt(color,16)
 	));
@@ -20,17 +20,17 @@ export function hexToRGB(hexColor){
 
 // Take in a rgb array and convert it to a hex string
 export function RGBToHex( rgbColor ){
-	let hexValues = rgbColor.map( ( colorPart ) => (
+	const hexValues = rgbColor.map( ( colorPart ) => (
 		colorPart.toString(16).toUpperCase()
 	));
 
 	return hexValues.reduce((acc, val) => (
-		acc + (val.length == 1 ? 0+val : val)
+		acc + (val.length === 1 ? 0+val : val)
 	));
 }
 
 // Combine two arrays so you end up with a single array of arrays
-// [A,B,C] and [1,2,3] will result in [A1,B2,C3]
+// [A,B,C] and [1,2,3] will result in [[A,1],[B,2],[C,3]]
 export function zipArrays(a1,a2){
 	return a1.map(( e , i ) => (
 		[a1[i],a2[i]]
@@ -39,9 +39,9 @@ export function zipArrays(a1,a2){
 
 //Take two colors and a weight and return a single color
 export function mixColors( color1, color2, weight ) {
-	let weight2 = 1 - weight;
+	const weight2 = 1 - weight;
 
-	let rgbCombined = zipArrays( hexToRGB( color1 ), hexToRGB( color2 ) ).map( ( [ a, b] ) => (
+	const rgbCombined = zipArrays( hexToRGB( color1 ), hexToRGB( color2 ) ).map( ( [ a, b] ) => (
 		Math.round( a * weight + b * weight2 )
 	) );
 	return RGBToHex(rgbCombined);
